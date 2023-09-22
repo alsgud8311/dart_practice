5910da471238e11266428f580d526dd61105bc4b
Minhyung Cho <dup33333@hufs.ac.kr>
2023-09-23 02:51:35 +0900

    Practice: const variable

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -3,6 +3,6 @@ import 'package:dart_application_1/dart_application_1.dart'
 
 bool isEmpty(String string) => string.length == 0;
 void main() {
-  late final String mh;
-  mh = 12;
+  const name = "mh";
+  name = 12;
 }
