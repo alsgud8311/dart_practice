7a5c5f0e53ddee2a36e5c5e274a4d6d665e59c2a
Minhyung Cho <dup33333@hufs.ac.kr>
2023-09-23 02:40:17 +0900

    Practice: Null safety

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,10 +1,11 @@
 import 'package:dart_application_1/dart_application_1.dart'
     as dart_application_1;
 
+bool isEmpty(String string) => string.length == 0;
 void main() {
-  dynamic name;
-  name = "mh";
-  name = 12;
-  name = true;
-  print(name);
+  String? mh = "mh";
+  mh = null;
+  if (mh != null) {
+    mh?.isNotEmpty;
+  }
 }
