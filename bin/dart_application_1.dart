eb81c9665a6b1fe78c9fae9f0e090bbdb896d424
Minhyung Cho <dup33333@hufs.ac.kr>
2023-09-23 14:25:54 +0900

    Practice: sets

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -3,29 +3,12 @@ import 'package:dart_application_1/dart_application_1.dart'
 
 bool isEmpty(String string) => string.length == 0;
 void main() {
-  var player = {
-    'name': 'minsu',
-    'power': true,
-    'age': 21,
+  Set<int> nums = {
+    1,
+    2,
+    3,
+    4,
   };
-
-  Map<int, bool> play = {
-    1: true,
-    2: false,
-    3: true,
-  };
-
-  Map<List<int>, bool> ages = {
-    [1, 2, 3, 4, 5]: true,
-  };
-
-  List<Map<String, Object>> players = [
-    {'name': 'mh', 'xp': 121232134},
-    {'name': 'minsu', 'xp': 1234124124},
-  ];
-
-  print(players);
-  print(player);
-  print(play);
-  print(ages);
+  nums.add(666);
+  print(nums);
 }
