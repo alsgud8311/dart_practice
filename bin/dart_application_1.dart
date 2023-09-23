23b0179d5dfd2a42a32996b1f50520d72df01c18
Minhyung Cho <dup33333@hufs.ac.kr>
2023-09-23 13:58:55 +0900

    Practice: Collection for

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -3,9 +3,16 @@ import 'package:dart_application_1/dart_application_1.dart'
 
 bool isEmpty(String string) => string.length == 0;
 void main() {
-  var mh = "minhyung";
-  var age = 10;
-  var greeting =
-      "hello, my name is $mh, and I'm ${age + 14} years old. nice to meet you";
-  print(greeting);
+  var oldFriends = [
+    'nico',
+    'minsu',
+  ];
+  var newFriends = [
+    'minhyung',
+    'louis',
+    'ralph',
+    for (var friend in oldFriends) "damn $friend",
+  ];
+
+  print(newFriends);
 }
