5e6b899dc4488732acef7e22f637f8151e17fc43
Minhyung Cho <dup33333@hufs.ac.kr>
2023-09-23 17:30:31 +0900

    Practice: inheritance

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,45 +1,29 @@
 import 'package:dart_application_1/dart_application_1.dart'
     as dart_application_1;
 
-abstract class Human {
-  void walk();
-}
-
-enum Team { red, blue }
-
-enum XPLevel { beginner, medium, pro }
-
-class Player extends Human{
-  String name;
-  XPLevel xp;
-  Team team;
-
-  Player({
-    required this.name,
-    required this.xp,
-    required this.team,
-  });
-
-  Player.fromJson(Map<String, dynamic> playerJson)
-      : name = playerJson['name'],
-        xp = playerJson['xp'],
-        team = playerJson['team'];
-
+class Human {
+  final String name;
+  Human({required this.name});
   void sayHello() {
-    print("hi my name is $name");
-  }
-
-  void walk(){
-    print('walking...')
+    print('Hi. My name is $name');
   }
 }
 
-class Coach extends Human {
-  void walk(){
-    print('walking coach')
+enum Team { blue, red }
+
+class Player extends Human {
+  final Team team;
+
+  Player({required this.team, required String name}) : super(name: name);
+
+  @override
+  void sayHello() {
+    super.sayHello();
+    print('and I play for ${team}');
   }
 }
 
 void main() {
-
+  var player = Player(team: Team.red, name: 'mh');
+  player.sayHello();
 }
