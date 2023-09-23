39a6b56e3221b41838890efe64a46cb581d37e35
Minhyung Cho <dup33333@hufs.ac.kr>
2023-09-23 13:55:43 +0900

    Practice: String Interpolation

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -3,21 +3,9 @@ import 'package:dart_application_1/dart_application_1.dart'
 
 bool isEmpty(String string) => string.length == 0;
 void main() {
-  var give = true;
-  var numbers = [
-    1,
-    2,
-    3,
-    4,
-    5,
-    if (give) 124214,
-  ];
-  List<int> nums = [
-    1,
-    3,
-    6,
-    8,
-  ];
-  nums.add(29);
-  print(numbers);
+  var mh = "minhyung";
+  var age = 10;
+  var greeting =
+      "hello, my name is $mh, and I'm ${age + 14} years old. nice to meet you";
+  print(greeting);
 }
