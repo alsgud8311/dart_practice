4ff89b8abc765e0b295484fe54d7c04ad58d7e76
Minhyung Cho <dup33333@hufs.ac.kr>
2023-09-23 14:19:38 +0900

    Practice: Maps

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -3,16 +3,29 @@ import 'package:dart_application_1/dart_application_1.dart'
 
 bool isEmpty(String string) => string.length == 0;
 void main() {
-  var oldFriends = [
-    'nico',
-    'minsu',
-  ];
-  var newFriends = [
-    'minhyung',
-    'louis',
-    'ralph',
-    for (var friend in oldFriends) "damn $friend",
+  var player = {
+    'name': 'minsu',
+    'power': true,
+    'age': 21,
+  };
+
+  Map<int, bool> play = {
+    1: true,
+    2: false,
+    3: true,
+  };
+
+  Map<List<int>, bool> ages = {
+    [1, 2, 3, 4, 5]: true,
+  };
+
+  List<Map<String, Object>> players = [
+    {'name': 'mh', 'xp': 121232134},
+    {'name': 'minsu', 'xp': 1234124124},
   ];
 
-  print(newFriends);
+  print(players);
+  print(player);
+  print(play);
+  print(ages);
 }
