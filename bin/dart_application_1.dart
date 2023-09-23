c38c801278c135a6ce6990f420978e565ad644a0
Minhyung Cho <dup33333@hufs.ac.kr>
2023-09-23 16:12:31 +0900

    Practice: constructure

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -2,8 +2,10 @@ import 'package:dart_application_1/dart_application_1.dart'
     as dart_application_1;
 
 class Player {
-  final String name = 'minhyung';
-  int xp = 1500;
+  final String name;
+  int xp;
+
+  Player(this.name, this.xp);
 
   void sayHello() {
     print("hi my name is $name");
@@ -11,7 +13,8 @@ class Player {
 }
 
 void main() {
-  var player = new Player();
-  print(player.name);
+  var player = Player("minhyung", 1200);
   player.sayHello();
+  var player2 = Player('minsu', 1202);
+  player2.sayHello();
 }
