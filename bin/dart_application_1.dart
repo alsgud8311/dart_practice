102e54d78bb99497e2213af742d5ea10c6a52c22
Minhyung Cho <dup33333@hufs.ac.kr>
2023-09-23 14:32:16 +0900

    Practice: functions

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,14 +1,19 @@
 import 'package:dart_application_1/dart_application_1.dart'
     as dart_application_1;
 
-bool isEmpty(String string) => string.length == 0;
+void sayHello(String name) {
+  print("Hello $name. nice to meet you");
+}
+
+String sayHelloname(String name) {
+  return "Hello $name. nice to meet you";
+}
+
+String fatArrow(String name) => "Hello $name. nice to meet you";
+
 void main() {
-  Set<int> nums = {
-    1,
-    2,
-    3,
-    4,
-  };
-  nums.add(666);
-  print(nums);
+  var name = 'minsu';
+  sayHello(name);
+  print(sayHelloname('minhyung'));
+  print(fatArrow('byeongju'));
 }
