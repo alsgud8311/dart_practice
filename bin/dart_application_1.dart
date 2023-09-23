289967b048c5a46ea82b4d65aed93950fd3303df
Minhyung Cho <dup33333@hufs.ac.kr>
2023-09-23 15:09:34 +0900

    optional positional parameters

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,18 +1,9 @@
 import 'package:dart_application_1/dart_application_1.dart'
     as dart_application_1;
 
-String sayHello({
-  required String name,
-  required int age,
-  required String country,
-}) {
-  return "Hello $name. you are $age and you come from $country.";
-}
+String sayHello(String name, int age, [String? country = "cheongju"]) =>
+    'Hello $name, you are $age and you are living in $country.';
 
 void main() {
-  print(sayHello(
-    name: 'minsu',
-    age: 12,
-    country: 'cheongju',
-  ));
+  print(sayHello('mh', 24));
 }
