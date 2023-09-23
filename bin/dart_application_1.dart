528c216142283195cecb7e6820147db690c5a3f3
Minhyung Cho <dup33333@hufs.ac.kr>
2023-09-23 17:37:46 +0900

    Practice: Mixins

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,6 +1,18 @@
 import 'package:dart_application_1/dart_application_1.dart'
     as dart_application_1;
 
+class Strong {
+  final double strengthLevel = 1500.999;
+}
+class QuickRunner{
+  void runQuick{
+    print("run!!!!!!!!!")
+  }
+}
+class Tall {
+  final double height = 1.77;
+  }
+
 class Human {
   final String name;
   Human({required this.name});
@@ -8,10 +20,14 @@ class Human {
     print('Hi. My name is $name');
   }
 }
+class Horse with Strong, QuickRunner{
+}
 
+class Kid with QuickRunner{
+}
 enum Team { blue, red }
 
-class Player extends Human {
+class Player with Strong, QuickRunner, Tall {
   final Team team;
 
   Player({required this.team, required String name}) : super(name: name);
@@ -25,5 +41,5 @@ class Player extends Human {
 
 void main() {
   var player = Player(team: Team.red, name: 'mh');
-  player.sayHello();
+  player.runQuick();
 }
