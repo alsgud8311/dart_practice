a962101d0d819f4ad95fcd59378f00669cba2de8
Minhyung Cho <dup33333@hufs.ac.kr>
2023-09-23 15:16:01 +0900

    Practice: Operator

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,9 +1,9 @@
 import 'package:dart_application_1/dart_application_1.dart'
     as dart_application_1;
 
-String sayHello(String name, int age, [String? country = "cheongju"]) =>
-    'Hello $name, you are $age and you are living in $country.';
+String capitalizeName(String? name) => name?.toUpperCase() ?? 'ANON';
 
 void main() {
-  print(sayHello('mh', 24));
+  print(capitalizeName('minhyung'));
+  print(capitalizeName(null));
 }
